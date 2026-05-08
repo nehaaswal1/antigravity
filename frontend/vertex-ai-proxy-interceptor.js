1d38b56c0368fa2f35d9ca0fd9e5a77b985c4da5
nehaaswal1 <86961876+nehaaswal1@users.noreply.github.com>
2026-05-08 14:13:52 +0530

    travel

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'NvjavqHxzXtlaP4ueZhp5lYqgLseGCYJ',
+            'X-App-Proxy': 'TVsaTwpQSrKSpLcHapObqagIChGbgTMF',
           },
           body: JSON.stringify(requestDetails),
         };
